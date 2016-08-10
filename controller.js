139aae1e7f5c86a88c3200c10d5d4ee43ed0735a
dirkhudsucker <dirkhudsucker@gmail.com>
2016-08-10 05:13:10 -0400

    a little progress

--- controller.js
+++ controller.js
@@ -1,14 +1,16 @@
-var mapcontrol = angular.module('mapcontrol', []);
-mapcontrol.controller('AppCtrl', ['$scope', '$http','$timeout', function($scope, $http,$timeout) {
-    console.log("Hello World from controller");
-    //loading data
-var refresh=function(){
-$http.get('/vehicles').success(function(response){
-      console.log("i got the data");
-      $scope.vehicles=response;
-      //init map
-      $timeout(function(){          
-            var latlng = new google.maps.LatLng(45.518,-122.672);
+var mapcontrol = angular.module('mapcontrol', ['angular.filter']);
+var markers=[];
+ var image = {
+    url: 'image/bus.png',
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(32, 32),
+     anchor: new google.maps.Point(16, 16)
+   
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    
+  };
+var infowindow = new google.maps.InfoWindow();
+var latlng = new google.maps.LatLng(45.518,-122.672);
             var myOptions = {
                 zoom: 13,
                 center: latlng,
@@ -20,32 +22,73 @@ $http.get('/vehicles').success(function(response){
         google.maps.MapTypeId.TERRAIN
       ],
             };
-            $scope.map = new google.maps.Map(document.getElementById("map"), myOptions); 
-            $scope.overlay = new google.maps.OverlayView();
-            $scope.overlay.draw = function() {}; // empty function required
-            $scope.overlay.setMap($scope.map);
-            $scope.element = document.getElementById('map');
-
-            //markers
-            for(i=0;i<$scope.vehicles.length;i++){
-           var obj=$scope.vehicles[i];
+            var map = new google.maps.Map(document.getElementById("map"), myOptions); 
+mapcontrol.controller('AppCtrl', ['$scope', '$http','$timeout', function($scope, $http,$timeout) {
+    console.log("Hello World from controller");
+    //loading data
+var refresh=function(){
+$http.get('/vehicles').success(function(response){
+      $scope.vehicles=response;
+      for(i=0;i<$scope.vehicles.length;i++){
+        var obj=$scope.vehicles[i];
+        
         var marker = new google.maps.Marker({
       position: new google.maps.LatLng(obj.latitude,obj.longitude),
-      map: $scope.map,
+      icon: image,
+      map: map,
      });
+     markers.push(marker);
+      var details = "<h5>RouteID:"+" "+"<mark>"+obj.routeNumber+"</mark>"+"</h5>"
+      +"<h5>Direction:"+" "+"<mark>"+obj.direction+"</mark>"+"</h5>"
+      +"<h5>Next Stop:"+" "+"<mark>"+obj.nextLocID+"</mark>"+"</h5>"
+      +"<h5>Next Stop:"+" "+"<mark>"+obj.longitude+"</mark>"+"</h5>";
+        bindInfoWindow(marker, map, infowindow, details);
+
+     
+
+
 
 };
 
-          
-        },100);
-console.log($scope.vehicles[1].latitude);
+  function bindInfoWindow(marker, map, infowindow, strDescription) {
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(strDescription);
+        infowindow.open(map, marker);
+    });
+   
+}   
+      
+
 
 
 });
 }
+
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
 refresh();
+ $scope.select = {
+    rate: null,
+    names:['3000','6000','9000','10000']
+   };
+
+
+$scope.refreshrate=function(){
+clearInterval($scope.handle);
+if($scope.select.rate<9000){      
+$scope.handle=setInterval(function(){   console.log($scope.select.rate); setMapOnAll(null); markers=[]; refresh(); }, $scope.select.rate);
+
+}
+
+};
 
 
 
 }]);
+
+
+
 
